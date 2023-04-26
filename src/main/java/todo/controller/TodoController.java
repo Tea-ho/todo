09a3b54d521e33e771e1e09d70d92f441f1a562e
Tea-ho <hokeng92@naver.com>
2023-04-26 09:50:44 +0900

    23.04.26, 09:50 설명 필요한 부분 주석처리

--- src/main/java/todo/controller/TodoController.java
+++ src/main/java/todo/controller/TodoController.java
@@ -17,15 +17,30 @@ import java.util.stream.Collectors;
 @RequestMapping("/todo") @Slf4j
 public class TodoController {
 
-    @Autowired
-    private TodoService todoService;
+    @Autowired private TodoService todoService;
 
     @GetMapping("/list.do") @CrossOrigin(origins="http://localhost:3000")
     public ResponseEntity<?> retrieveTodoList(@AuthenticationPrincipal String userId){
         // 1. 서비스 메소드의 retrieve 메소드를 사용해서 todo 리스트 가져오기
         List<TodoEntity> entities = todoService.retrieve(userId);
+
+        // 2. 자바 스트림을 이용하여 리턴된 엔티티 리스트 todoDto 리스트로 변환
         List<TodoDto> dtos = entities.stream().map(TodoDto::new).collect(Collectors.toList());
+        // 해석1: entities type은 List<TodoEntity>으로, TodoEntity 객체들이 담긴 리스트를 뜻함
+        // 해석2: 이 리스트를 stream() 메소드를 사요아여 스트림으로 변환
+        // 해석3: 이후 map() 메소드를 이용하여 스트림 안의 모든 요소에 대해 TodoDto::new 생성자 참조를 적용
+        // 해석4: 이 작업을 통해 TodoDto 생성자를 호출하여 TodoEntity 객체를 TodoDto 객체로 변환 진행
+        // 해석5: collect() 메소드를 사용하여 변환된 TodoDto 객체를 List<TodoDto> 타입으로 수집
+        // 해석6: toList() 메소드를 사용하여 리스트로 수집할 수 있음.
+        // 요약: entities 리스트를 스트림으로 변환하고,각 요소에 대해 TodoDto 생성자를 적용하여 TodoEntity 객체를 TodoDto객체로 변환한 뒤,
+        // 그 결과를 List<TodoDto>로 수집하여 반환
+
+        // 3. 변환된 todoDto 리스트를 이용하여 ResponseDTO 초기화 진행
         ResponseDTO<TodoDto> response = ResponseDTO.<TodoDto>builder().data(dtos).build();
+            System.out.println("list.do result");
+            System.out.println(ResponseEntity.ok(response));
+
+        // 4. ResponseDTO 반환
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/todo/service/TodoService.java
+++ src/main/java/todo/service/TodoService.java
@@ -17,29 +17,75 @@ public class TodoService {
     @Autowired
     TodoEntityRepository todoEntityRepository;
 
-    @Transactional
-    public List<TodoEntity> create(final TodoEntity entity) {
-        validate(entity);
-        todoEntityRepository.save(entity);
-        log.info("service create : " + entity.getId());
-        return todoEntityRepository.findByUserId(entity.getUserId());
-    }
-
+    // 0. 유효성 검사 메소드
     private void validate(final TodoEntity entity) { // 유효성 검사
-
-        if(entity==null){
+        if(entity==null){ // empty entity checking
             log.info("Entity cannot be null.");
             throw new RuntimeException("Entity cannot be null.");
         }
-
-        if(entity.getUserId()==null){
+        if(entity.getUserId()==null){ // getUserId null 상태 checking
             log.info("Unknown user.");
             throw new RuntimeException("Unknown user.");
         }
-
     }
 
-    //할일 등록
+    // 1. create 메소드 (기능: todo 입력내용 entity에 저장)
+    @Transactional
+    public List<TodoEntity> create(final TodoEntity entity) {
+        validate(entity);
+        // 해석: 들어온 entity 유효성 검사 진행
+        todoEntityRepository.save(entity);
+        // 해석: entity Repository에 저장(DB저장)
+        log.info("service create : " + entity.getId());
+        return todoEntityRepository.findByUserId(entity.getUserId());
+        // 저장된 값 반환
+    }
+
+    // 2. 반환하는 메소드 (기능: userId 받아서 일치하는 entity 반환)
+    @Transactional
+    public List<TodoEntity> retrieve(final String userId){
+        return todoEntityRepository.findByUserId(userId);
+    }
+
+
+
+    // 3. 업데이트 메소드 (기능: entity 받아서 변경된 내용 적용)
+    @Transactional
+    public List<TodoEntity> updateTodo(final TodoEntity entity){
+
+        validate(entity);
+        // 해석: 유효성 검사 진행
+
+        final Optional<TodoEntity> todoEntityOptional = todoEntityRepository.findById(entity.getId());
+        // 해석: null일 때 발생되는 오류 방지하기 위해 포장지로 감싸는 작업 진행
+        
+        todoEntityOptional.ifPresent(todo -> {
+        // 해석: 존재한다면(null이 아니라면), 아래 코드 실행(entity 초기화 작업 및 repository(DB) 수정 작업)
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+            todoEntityRepository.save(todo);
+        });
+        return retrieve(entity.getUserId());
+        // 해석: 데이터 변경 후 랜더링 진행
+    }
+    // 4. 삭제 메소드 (기능: entity 정보와 일치하는 entity 삭제)
+    @Transactional
+    public List<TodoEntity> deleteTodo(final TodoEntity entity){
+        validate(entity);
+        // 해석: 유효성 검사 진행
+
+        try{ // 해석: 유효성 검사 결과 적합하면, deleting 작업 진행
+            todoEntityRepository.delete(entity);
+        } catch (Exception e){
+            log.info("service delete : " + e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        } // 해석: 오류 발생 시, 오류 메시지 log로 남기고, RuntimeException에 던지기 처리
+        return retrieve(entity.getUserId());
+        // 해석: entity 삭제처리하고 랜더링 진행
+    }
+
+    // 2. writetodo 메소드 할일 등록 (사용하지 않는 메소드)
+    /*
     @Transactional
     public boolean writeTodo(TodoDto todoDto){
         log.info("service todo post : " +todoDto );
@@ -50,41 +96,5 @@ public class TodoService {
         }
 
         return false;
-    }
-
-    //
-    @Transactional
-    public List<TodoEntity> retrieve(final String userId){
-        return todoEntityRepository.findByUserId(userId);
-    }
-
-
-
-    //할일 수정
-    @Transactional
-    public List<TodoEntity> updateTodo(final TodoEntity entity){
-
-        validate(entity); // 유효성 검사 진행
-
-        final Optional<TodoEntity> todoEntityOptional = todoEntityRepository.findById(entity.getId());
-        todoEntityOptional.ifPresent(todo -> {
-
-            todo.setTitle(entity.getTitle());
-            todo.setDone(entity.isDone());
-            todoEntityRepository.save(todo);
-        });
-        return retrieve(entity.getUserId());
-    }
-    //할일 삭제
-    @Transactional
-    public List<TodoEntity> deleteTodo(final TodoEntity entity){
-        validate(entity); // 유효성 검사 진행
-        try{
-            todoEntityRepository.delete(entity);
-        } catch (Exception e){
-            log.info("service delete : " + e.getMessage());
-            throw new RuntimeException(e.getMessage());
-        }
-        return retrieve(entity.getUserId());
-    }
+    }*/
 }
